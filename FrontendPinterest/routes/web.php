68ed3b8887f42054c76f28ec34a98c81d50252cc
Alejandra Solano <alejandra133valle@gmail.com>
2024-07-23 16:37:14 -0600

    cambio nombre

--- FrontendPinterest/routes/web.php
+++ FrontendPinterest/routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/inicio', function () {
-    return view('inicio');
+Route::get('/login', function () {
+    return view('login');
 });
